85e42da427ee207300f87e3f4b11fa88543cf436
shahin <me@shahinnazarov.com>
2021-05-03 22:23:51 +0400

    email changed docker implg

--- etaskify/src/main/java/az/etaskify/service/EmailServiceImpl.java
+++ etaskify/src/main/java/az/etaskify/service/EmailServiceImpl.java
@@ -1,5 +1,7 @@
 package az.etaskify.service;
 
+import az.etaskify.exception.EmailException;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
@@ -7,29 +9,26 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class EmailServiceImpl implements EmailService {
-
-
     private final JavaMailSender emailSender;
     private final Environment environment;
 
     @Override
-    public void sendMail(String to, String subject, String text) {
+    public void sendMail(String subject, String text, String... recipients) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(Objects.requireNonNull(environment.getProperty("spring.mail.username")));
-            message.setTo(to);
+            message.setTo(recipients);
             message.setSubject(subject);
             message.setText(text);
             emailSender.send(message);
-            log.debug("Success sending mail to {} .", to);
+            log.debug("Email with {} title was sent {} recipients.", subject, recipients.length);
         } catch (Exception e) {
-            log.error("Fail sending mail to {} .", to, e);
+            log.debug("Sending an email with {} title was failed.", subject);
+            throw new EmailException("Failed to send an email");
         }
 
     }
